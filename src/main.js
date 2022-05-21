e0f367c04f44b04e09f772097843b42c48c0df89
unknown <bobo>
2022-05-21 17:40:44 +0800

    引入项目组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vamt 全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
+
+// 注册使用vant
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
